5861282e87b73c0770063cd80fd3a34102b9e796
Dmitry Kabanov <dkabanov6@gmail.com>
2019-02-24 12:10:52 +0500

    added test without snapshot

--- __tests__/test.tabs.jsx
+++ __tests__/test.tabs.jsx
@@ -1,35 +1,83 @@
+import 'react-log-state';
 import App from '../src/components/App';
 
-const tabsData = [
-  {
-    id: 0,
-    title: 'Tab 1',
-    text: 'Tab text 1',
-  },
-  {
-    id: 1,
-    title: 'Tab 2',
-    text: 'Tab text 2',
-  },
-  {
-    id: 2,
-    title: 'Tab 3',
-    text: 'Tab text 3',
-  },
-];
+ReactLogState.logAll();
+
+const tabControlsSelector = 'li[data-test="tab-control"]';
+const addNewTabBtnSelector = '[data-test="add-tab-btn"]';
+const removeTabBtnSelector = '[data-test="remove-tab-btn"]';
+const tabControlsContainerSelector = 'ul[data-test="tab-controls-container"]';
+const tabSelectedSelector = '[aria-selected="true"]';
+const tabUnselectedSelector = '[aria-selected="false"]';
+
+const getSelector = app => ({
+  getTabControlList: () => app.find(tabControlsSelector),
+  getTabControsContainer: () => app.find(tabControlsContainerSelector),
+  getTabByIndex: index => app.find(tabControlsSelector).at(index),
+  getAddNewTabBtn: () => app.find(addNewTabBtnSelector),
+  getRemoveTabBtn: () => app.find(removeTabBtnSelector),
+});
 
 describe('Tabs snapshot', () => {
   test('Renders', () => {
-    const wrapper = mount(<App tabs={tabsData} />);
-    expect(wrapper.render()).toMatchSnapshot();
+    const app = mount(<App />);
+    expect(app.render()).toMatchSnapshot();
   });
 
   test('Click to second tab', () => {
-    const wrapper = mount(<App tabs={tabsData} />);
-    const tabControls = wrapper.find('[data-test="tab-control"]');
-    const secondTab = tabControls.at(2);
+    const app = mount(<App />);
+    const s = getSelector(app);
+    const secondTab = s.getTabByIndex(1);
 
     secondTab.simulate('click');
-    expect(wrapper.render()).toMatchSnapshot();
+    expect(app.render()).toMatchSnapshot();
+  });
+});
+
+describe('Tabs without snapshot', () => {
+  test('Correct renders tab', () => {
+    const app = mount(<App />);
+    const s = getSelector(app);
+    const firstTab = s.getTabByIndex(0);
+    const secondTab = s.getTabByIndex(1);
+
+    expect(firstTab).toMatchSelector(tabSelectedSelector);
+    expect(secondTab).toMatchSelector(tabUnselectedSelector);
+  });
+
+  test('Correct switching to second tab', () => {
+    const app = mount(<App />);
+    const s = getSelector(app);
+    const secondTab = s.getTabByIndex(1);
+
+    secondTab.simulate('click');
+
+    const firstTab = s.getTabByIndex(0);
+    const updatedSecondTab = s.getTabByIndex(1);
+
+    expect(firstTab).toMatchSelector(tabUnselectedSelector);
+    expect(updatedSecondTab).toMatchSelector(tabSelectedSelector);
+  });
+
+  test('Correct adding new tab', () => {
+    const app = mount(<App />);
+    const s = getSelector(app);
+    const addNewTabBtn = s.getAddNewTabBtn();
+
+    addNewTabBtn.simulate('click');
+
+    const tabControlsContainer = s.getTabControsContainer();
+    expect(tabControlsContainer).toContainMatchingElements(4, tabControlsSelector);
+  });
+
+  test('Correct removal tab', () => {
+    const app = mount(<App />);
+    const s = getSelector(app);
+    const removeTabBtn = s.getRemoveTabBtn();
+
+    removeTabBtn.simulate('click');
+
+    const tabControlsContainer = s.getTabControsContainer();
+    expect(tabControlsContainer).toContainMatchingElements(2, tabControlsSelector);
   });
 });
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -6,8 +6,29 @@ import {
   TabList,
   TabPanel,
 } from 'react-tabs';
+import { uniqueId } from 'lodash';
 
 class Tabs extends Component {
+  state = {
+    tabs: [
+      { id: uniqueId(), title: 'Tab 1', text: 'Tab text 1' },
+      { id: uniqueId(), title: 'Tab 2', text: 'Tab text 2' },
+      { id: uniqueId(), title: 'Tab 3', text: 'Tab text 4' },
+    ],
+  }
+
+  onAddNewTab = () => {
+    const { tabs } = this.state;
+    const nextId = uniqueId();
+    const newTab = { id: nextId, title: `Tab ${nextId}`, text: `Tab text ${nextId}` };
+    this.setState({ tabs: [...tabs, newTab] });
+  }
+
+  onRemoveTab = tabId => () => {
+    const { tabs } = this.state;
+    this.setState({ tabs: tabs.filter(({ id }) => id !== tabId) });
+  }
+
   renderTab = tabs => tabs.map(({ id, title }) => (
     <Tab data-test="tab-control" key={id}>
       {title}
@@ -17,14 +38,20 @@ class Tabs extends Component {
   renderTabPanel = tabs => tabs.map(({ id, text }) => (
     <TabPanel key={id}>
       {text}
+      <button type="button" data-test="remove-tab-btn" onClick={this.onRemoveTab(id)}>
+        {`Remove tab ${id}`}
+      </button>
     </TabPanel>
   ))
 
   render() {
-    const { tabs } = this.props;
+    const { tabs } = this.state;
     return (
       <TabsWrapper>
-        <TabList>
+        <button type="button" data-test="add-tab-btn" onClick={this.onAddNewTab}>
+          Add tab
+        </button>
+        <TabList data-test="tab-controls-container">
           {this.renderTab(tabs)}
         </TabList>
         <React.Fragment>
